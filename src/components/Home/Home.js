e6c851a98c6c33d38608c161a99453f533f3f542
Marika Mäki <zeracheciel@gmail.com>
2023-03-18 11:58:10 +0200

    Updated home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../store_context/auth-context';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
-const Home = (props) => {
+const Home = () => {
+  const ctx = useContext(AuthContext);
+
   return (
     <Card className={classes.home}>
-      <h1>Welcome back!</h1>
-    </Card>
-  );
-};
+      <h1>Welcome back, User!</h1>
+      {ctx.isLoggedIn && (
+      <button onClick={ctx.onLogout}>Log out</button>
+      )}
+      </Card>
+  )};
 
 export default Home;
